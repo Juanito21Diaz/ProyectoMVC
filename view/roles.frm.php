974b9250cf17ae4481681872c64e80b700d437f1
Juanito21Diaz <juancho77721@gmail.com>
2023-04-24 11:41:38 -0500

    Archivos header y footer, formulario roles

---
+++ view/roles.frm.php
@@ -0,0 +1,19 @@
+<?php include_once "header.php"; //trae el contenido del encabezado ?>
+<form action="#" method="post" id="rolesFrm">
+    <div class="row my-5">
+        <div class="col-12">
+            <h1 class="text-center">Formulario Roles</h1>
+        </div>
+    </div>
+    <div class="row d-flex justify-content-end">
+        <div class="col-6">
+            <label for="" class="form-label">Nombre del Rol: </label>
+            <input type="text" class="form-control" id="txtNombre" name="txtNombre">
+        </div>
+        <div class="col-3 align-self-end">
+            <input class="btn btn-dark bg-dark" type="submit" value="Registrar">
+        </div>
+    </div>
+</form>
+<?php include_once "footer.php";//trae el contenido del pie de pagina ?>
+<script src="./assets/js/roles.js"></script>
